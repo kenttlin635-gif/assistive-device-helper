dec9000ed1d8404ba2e63d524110fd93117aefc4
kenttlin635-gif <kenttlin635@gmail.com>
2026-01-05 14:35:35 +0800

    Add files via upload

---
+++ assistive-device-helper/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'subsidy-search-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json',
+  // 這裡快取外部 CDN，確保離線也能執行 React
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
